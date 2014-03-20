c7647cf5626bae18f5367863c03377a220ebcbd7
Ralph Bean <rbean@redhat.com>
2014-03-20 09:53:36 -0400

    Reorganization.

--- bugzilla2fedmsg.py
+++ bugzilla2fedmsg.py
@@ -15,106 +15,154 @@ import fedmsg
 import moksha.hub.api
 import moksha.hub.reactor
 
+# These are bug fields we're going to try and pass on to fedmsg.
+bug_fields = [
+    'alias',
+    'assigned_to',
+    'attachments',
+    'blocks',
+    'cc',
+    'classification',
+    'comments',
+    'component',
+    'components',
+    'creation_time',
+    'creator',
+    'depends_on',
+    'description',
+    'docs_contact',
+    'estimated_time',
+    'external_bugs',
+    'fixed_in',
+    'flags',
+    'groups',
+    'id',
+    'is_cc_accessible',
+    'is_confirmed',
+    'is_creator_accessible',
+    'is_open',
+    'keywords',
+    'last_change_time',
+    'op_sys',
+    'platform',
+    'priority',
+    'product',
+    'qa_contact',
+    'actual_time',
+    'remaining_time',
+    'resolution',
+    'see_also',
+    'severity',
+    'status',
+    'summary',
+    'target_milestone',
+    'target_release',
+    'url',
+    'version',
+    'versions',
+    'weburl',
+    'whiteboard',
+]
+
+
+def convert_datetimes(obj):
+    """ Recursively convert bugzilla DateTimes to stdlib datetimes. """
+
+    if isinstance(obj, list):
+        return [convert_datetimes(item) for item in obj]
+    elif isinstance(obj, dict):
+        return dict([
+            (k, convert_datetimes(v))
+            for k, v in obj.items()
+        ])
+    elif hasattr(obj, 'timetuple'):
+        timestamp = time.mktime(obj.timetuple())
+        return datetime.datetime.fromtimestamp(timestamp)
+    else:
+        return obj
+
 
 class BugzillaConsumer(moksha.hub.api.Consumer):
 
     # This is /topic/bugzilla in STOMP land.
     topic = '/topic/bugzilla'
 
-    bug_fields = [
-        'alias',
-        'assigned_to',
-        'attachments',
-        'blocks',
-        'cc',
-        'classification',
-        'comments',
-        'component',
-        'components',
-        'creation_time',
-        'creator',
-        'depends_on',
-        'description',
-        'docs_contact',
-        'estimated_time',
-        'external_bugs',
-        'fixed_in',
-        'flags',
-        'groups',
-        'id',
-        'is_cc_accessible',
-        'is_confirmed',
-        'is_creator_accessible',
-        'is_open',
-        'keywords',
-        'last_change_time',
-        'op_sys',
-        'platform',
-        'priority',
-        'product',
-        'qa_contact',
-        'actual_time',
-        'remaining_time',
-        'resolution',
-        'see_also',
-        'severity',
-        'status',
-        'summary',
-        'target_milestone',
-        'target_release',
-        'url',
-        'version',
-        'versions',
-        'weburl',
-        'whiteboard',
-    ]
-
     def __init__(self, hub):
         super(BugzillaConsumer, self).__init__(hub)
-        products = hub.config.get('bugzilla.products', 'Fedora, Fedora EPEL')
-        self.products = [product.strip() for product in products.split(',')]
 
         # Set up a queue to communicate between the main twisted thread
         # receiving stomp messages, and a worker thread that pulls items off
         # the queue, makes bz queries, and republishes to fedmsg.
-        self.queue = queue.Queue()
+        self.incoming = queue.Queue()
 
-        # Last thing we do is kick off our worker in a background thread.
-        moksha.hub.reactor.reactor.callInThread(self.worker)
-        self.log.info("Initialized bugzilla2fedmsg STOMP consumer.")
+        # Last thing we do is kick off our worker(s) in a background thread.
+        N = int(self.hub.config.get('bugzilla.num_workers', 1))
+        self.workers = []
+        for i in range(N):
+            # Share our queue, log and config with our workers
+            worker = WorkerThread(i, self.incoming, self.log, self.hub.config)
+            moksha.hub.reactor.reactor.callInThread(worker.work)
+            self.workers.append(worker)
+
+        self.log.info("Initialized bz2fm STOMP consumer with %i workers." % N)
 
     def consume(self, msg):
         """ Receive a STOMP message and put it on the queue for the worker """
         self.log.info("Main thread received %r.  Queueing." % msg)
-        self.queue.put([msg])
+        self.incoming.put(msg)
 
-    def worker(self):
+
+class WorkerThread(object):
+    def __init__(self, idx, incoming, log, config):
+        self.idx = idx
+        self.incoming = incoming
+        self.log = log
+        self.config = config
+
+        products = config.get('bugzilla.products', 'Fedora, Fedora EPEL')
+        self.products = [product.strip() for product in products.split(',')]
+
+    def debug(self, msg):
+        self.log.info("* thread #%i (backlog %i): %s" % (
+            self.idx, self.incoming.qsize(), msg))
+
+    def work(self):
         """ A worker thread that pulls stuff off the main thread's queue. """
 
-        self.log.info("Starting bugzilla2fedmsg worker thread.")
+        self.debug("Starting bz2fm worker.")
 
         # First, initialize fedmsg and bugzilla in this thread's context.
         hostname = socket.gethostname().split('.', 1)[0]
         fedmsg.init(name='bugzilla.%s' % hostname)
 
-        self.bugzilla = bugzilla.Bugzilla(url=self.hub.config.get(
+        self.bugzilla = bugzilla.Bugzilla(url=self.config.get(
             'bugzilla.url', 'https://bugzilla.redhat.com'))
 
         # Then, start working, forever.
-        self.log.info("bugzilla2fedmsg worker thread listening to the queue.")
-        for msg in self.queue.get():
+        self.debug("bz2fm worker thread waiting on incoming queue.")
+        while True:
+            # This is a blocking call.  It waits until a msg is available.
+            msg = self.incoming.get()
+            # TODO -- introduce some kind of sentinel value that can be used to
+            # tell this thread to exit cleanly.
             topic, msg = msg['topic'], msg['body']
-            self.log.info("Worker thread picking up %r" % msg)
-            self.handle(topic, msg)
+            self.debug("Worker thread picking up %r" % msg)
+            try:
+                self.handle(topic, msg)
+            except Exception as e:
+                self.log.exception(e)
+            self.debug("Going back to waiting on the incoming queue.")
+
+        self.debug("Thread exiting.")
 
     def handle(self, topic, msg):
-
         # First, look up our bug in bugzilla.
+        self.debug("Gathering metadata for #%s" % msg['bug_id'])
         bug = self.bugzilla.getbug(msg['bug_id'])
 
         # Drop it if we don't care about it.
         if bug.product not in self.products:
-            self.log.info("* DROP: %r not in %r" % (
+            self.debug("DROP: %r not in %r" % (
                 bug.product, self.products))
             return
 
@@ -123,9 +171,9 @@ class BugzillaConsumer(moksha.hub.api.Consumer):
         msg['timestamp'] = datetime.datetime.strptime(msg['timestamp'], fmt)
 
         # Find the event from the bz history that most likely corresponds here.
-        self.log.info("* Gathering history for #%s" % msg['bug_id'])
+        self.debug("Gathering history for #%s" % msg['bug_id'])
         history = bug.get_history()['bugs'][0]['history']
-        history = self.convert_datetimes(history)
+        history = convert_datetimes(history)
         event = self.find_relevant_event(msg, history)
 
         # If there are no events in the history, then this is a new bug.
@@ -133,14 +181,11 @@ class BugzillaConsumer(moksha.hub.api.Consumer):
         if not event:
             topic = 'bug.new'
 
-        self.log.info("* Gathering metadata for #%s" % msg['bug_id'])
-        bug = dict([
-            (field, getattr(bug, field, None))
-            for field in self.bug_fields
-        ])
-        bug = self.convert_datetimes(bug)
+        self.debug("Organizing metadata for #%s" % msg['bug_id'])
+        bug = dict([(attr, getattr(bug, attr, None)) for attr in bug_fields])
+        bug = convert_datetimes(bug)
 
-        self.log.info("* Republishing #%s" % msg['bug_id'])
+        self.debug("Republishing #%s" % msg['bug_id'])
         fedmsg.publish(
             modname='bugzilla',
             topic=topic,
@@ -169,20 +214,3 @@ class BugzillaConsumer(moksha.hub.api.Consumer):
                 best_delta = abs(best['when'] - msg['timestamp'])
 
         return best
-
-    @staticmethod
-    def convert_datetimes(obj):
-        """ Recursively convert bugzilla DateTimes to stdlib datetimes. """
-
-        if isinstance(obj, list):
-            return [BugzillaConsumer.convert_datetimes(item) for item in obj]
-        elif isinstance(obj, dict):
-            return dict([
-                (k, BugzillaConsumer.convert_datetimes(v))
-                for k, v in obj.items()
-            ])
-        elif hasattr(obj, 'timetuple'):
-            timestamp = time.mktime(obj.timetuple())
-            return datetime.datetime.fromtimestamp(timestamp)
-        else:
-            return obj
--- fedmsg.d/bugzilla2fedmsg-config.py
+++ fedmsg.d/bugzilla2fedmsg-config.py
@@ -3,8 +3,10 @@ hostname = socket.gethostname().split('.', 1)[0]
 
 config = {
     "endpoints": {
+        # You need as many of these as you have worker threads.
         'bugzilla.%s' % hostname: [
             'tcp://127.0.0.1:3032',
+            'tcp://127.0.0.1:3033',
         ],
     },
 }
