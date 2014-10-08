205074d7026c0e2eceb68292ae8c01e9afe8b31d
Ralph Bean <rbean@redhat.com>
2014-10-08 11:16:05 -0400

    Various bits to get this working.

--- the_new_hotness.py
+++ the_new_hotness.py
@@ -13,12 +13,18 @@ import fedmsg
 import fedmsg.consumers
 
 
-class BugzillaTicketFiler(fedmsg.consumers.Consumer):
+class BugzillaTicketFiler(fedmsg.consumers.FedmsgConsumer):
     topic = 'org.release-monitoring.prod.anitya.*'
-    config_key = 'the_new_hotness.bugzilla.enabled'
+    config_key = 'hotness.bugzilla.enabled'
 
     def __init__(self, hub):
         super(BugzillaTicketFiler, self).__init__(hub)
+
+        if not self._initialized:
+            return
+
+        # This is just convenient.
+        self.config = self.hub.config
 
         # First, initialize fedmsg and bugzilla in this thread's context.
         hostname = socket.gethostname().split('.', 1)[0]
@@ -32,7 +38,7 @@ class BugzillaTicketFiler(fedmsg.consumers.Consumer):
         self.log.info("Logging in to %s" % url)
         self.bugzilla.login(username, password)
 
-        self.log.info("Initialized that new hotness.")
+        self.log.info("That new hotness ticket filer is all initialized")
 
     def consume(self, msg):
         topic, msg = msg['topic'], msg['body']
