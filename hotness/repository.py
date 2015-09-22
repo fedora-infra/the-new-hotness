cc4b7f4f0a36d12de0c5ac89f43317bb56075e2e
Ralph Bean <rbean@redhat.com>
2015-09-22 11:10:49 -0400

    Try twice to find the rawhide version of packages.
    Sometimes our cache is just out of date.
    
    If we fail to find something, this forces a refresh.
    
    Should fix #40.

--- hotness/repository.py
+++ hotness/repository.py
@@ -7,7 +7,32 @@ log = logging.getLogger('fedmsg')
 
 def get_version(package_name, yumconfig):
     nvr_dict = build_nvr_dict(yumconfig)
-    return nvr_dict[package_name]
+    try:
+        version = nvr_dict[package_name]
+    except KeyError:
+        log.warn("Did not find %r in nvr_dict, forcing refresh" % package_name)
+        force_cache_refresh(yumconfig)
+        nvr_dict = build_nvr_dict(yumconfig)
+        # This might still fail.. but we did the best we could.
+        version = nvr_dict[package_name]
+    return version
+
+
+def force_cache_refresh(yumconfig):
+    # First, invalidate our in-memory cache of the results
+    cache.invalidate(hard=True)
+
+    # But also ask yum/dnf to kill its on-disk cache
+    cmdline = ["/usr/bin/yum",
+               "--config", yumconfig,
+               "clean",
+               "all"]
+    log.info("Running %r" % ' '.join(cmdline))
+    cleanall = subprocess.Popen(cmdline, stdout=subprocess.PIPE)
+    (stdout, stderr) = cleanall.communicate()
+    if stderr:
+        log.warn(stderr)
+    log.debug("Done with cache cleaning.")
 
 
 @cache.cache_on_arguments()
@@ -34,5 +59,7 @@ def build_nvr_dict(yumconfig):
         if line:
             name, version, release = line.split("\t")
             new_nvr_dict[name] = (version, release)
+
+    log.info("Rebuilt nvr_dict with %r entries" % len(new_nvr_dict))
 
     return new_nvr_dict
