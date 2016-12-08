7becc28c450c788c5a2b9a82b3acb54f6d92bd28
Jeremy Cline <jeremy@jcline.org>
2016-12-08 11:58:28 -0500

    Add a version to the hotness package
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup, find_packages
+import re
+
+
+def get_version():
+    """Get the current version of the hotness package"""
+    with open('hotness/__init__.py', 'r') as fd:
+        regex = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+        version = re.search(regex, fd.read(), re.MULTILINE).group(1)
+    if not version:
+        raise RuntimeError('No version set in hotness/__init__.py')
+    return version
+
 
 setup(
     name='the-new-hotness',
-    version='0.7.3',
+    version=get_version(),
     description='Consume anitya fedmsg messages to file bugzilla bugs',
     license='LGPLv2+',
     author='Ralph Bean',
