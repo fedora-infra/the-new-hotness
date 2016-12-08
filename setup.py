6a2a90cecd5c857f415bc9fbfca1c1689b86bb6b
Jeremy Cline <jeremy@jcline.org>
2016-12-08 15:26:24 -0500

    Add a requirements.txt and dev-requirements.txt
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+
+
+def get_requirements(requirements_file='requirements.txt'):
+    """Get the contents of a file listing the requirements.
+
+    :arg requirements_file: path to a requirements file
+    :type requirements_file: string
+    :returns: the list of requirements, or an empty list if
+              `requirements_file` could not be opened or read
+    :return type: list
+    """
+
+    lines = open(requirements_file).readlines()
+    return [
+        line.rstrip().split('#')[0]
+        for line in lines
+        if not line.startswith('#')
+    ]
+
 
 setup(
     name='the-new-hotness',
@@ -8,15 +28,8 @@ setup(
     author='Ralph Bean',
     author_email='rbean@redhat.com',
     url='https://github.com/fedora-infra/the-new-hotness',
-    install_requires=[
-        "fedmsg",
-        "python-bugzilla",
-        "dogpile.cache",
-        "requests",
-        "sh",
-        "six",
-        "fedmsg_meta_fedora_infrastructure",
-    ],
+    install_requires=get_requirements(),
+    tests_require=get_requirements('dev-requirements.txt'),
     packages=find_packages(),
     entry_points="""
     [moksha.consumer]
